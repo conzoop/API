562da446c73a0719458299f8b89f10a12efaf382
conzoop <connor.griffiths@roq.co.uk>
2023-11-18 13:53:24 +0000

    Negative Tests added

--- ConnorAPI/Drivers/DriverFactory.cs
+++ ConnorAPI/Drivers/DriverFactory.cs
@@ -4,8 +4,10 @@ using NUnit.Framework;
 using RestSharp;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Net;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,7 +19,7 @@ namespace ConnorAPI.Drivers
         string apiKey = "d67a55072cf473900fd3c72dddee7164";
         string apiToken = "ATTAd64501a55fecf791648769af2e4fe470942412a4af7193e54b73012d330082929097BAB1";
         string boardId = "6550e6aedd319dae007b75a7";
-        string todoListId = "YOUR_LIST_ID";
+        string todoListId = "65537ba06d40e52309e29636";
         string doneListId = "YOUR_LIST_ID_TWO";
         string cardId = "YOUR_CARD_ID";
 
@@ -34,24 +36,28 @@ namespace ConnorAPI.Drivers
         public void CreateNewBoard()
         {
             RestClient restClient = new RestClient(BaseUrl);
-            RestRequest restRequest = new RestRequest("boards/");
-            restRequest.AddQueryParameter("key", apiKey);
-            restRequest.AddQueryParameter("token", apiToken);
-            restRequest.AddQueryParameter("name", "ConnorsBoard");
-            RestResponse restResponse = restClient.Post(restRequest);
-            string BoardName = restResponse.Content.ToString();
-            var boardInfo = JsonConvert.DeserializeObject<Objects>(BoardName);
-            var board = boardInfo.Id;
+            RestRequest createBoardRequest = new RestRequest("boards/");
+            createBoardRequest.AddQueryParameter("key", apiKey);
+            createBoardRequest.AddQueryParameter("token", apiToken);
+            createBoardRequest.AddQueryParameter("name", "ConnorsBoard");
+            RestResponse createBoardResponse = restClient.Post(createBoardRequest);
+            var boardInfo = JsonConvert.DeserializeObject<Objects>(createBoardResponse.Content);
+            var boardId = boardInfo?.Id;
 
-            RestClient restClient2 = new RestClient(BaseUrl);
-            RestRequest restRequest2 = new RestRequest("boards/");
-            restRequest2.AddQueryParameter("key", apiKey);
-            restRequest2.AddQueryParameter("token", apiToken);
-            restRequest2.AddUrlSegment("id", board);
-            RestResponse restResponse2 = restClient.Get(restRequest);
-            HttpStatusCode = restResponse.StatusCode;
-            int statCode = (int)HttpStatusCode;
-            Assert.AreEqual(200, statCode);
+            RestRequest getBoardRequest = new RestRequest($"boards/{boardId}");
+            getBoardRequest.AddQueryParameter("key", apiKey);
+            getBoardRequest.AddQueryParameter("token", apiToken);
+
+            RestResponse getBoardResponse = restClient.Get(getBoardRequest);
+            string retrievedBoardDetails = getBoardResponse.Content.ToString();
+            var retrievedBoard = JsonConvert.DeserializeObject<BoardDetails>(retrievedBoardDetails);
+            string retrievedBoardId = retrievedBoard?.Id;
+            string retrievedBoardName = retrievedBoard?.Name;
+
+            Console.WriteLine($"Board ID: {retrievedBoardId}");
+            Console.WriteLine($"Board Name: {retrievedBoardName}");
+
+
         }
 
 
@@ -59,78 +65,149 @@ namespace ConnorAPI.Drivers
 
         public void CreateNewTODOList()
         {
-            RestClient restClient = new RestClient(BaseUrl);
-            RestRequest restRequest = new RestRequest("lists/");
-            restRequest.AddQueryParameter("key", apiKey);
-            restRequest.AddQueryParameter("token", apiToken);
-            restRequest.AddQueryParameter("name", "TODO");
-            restRequest.AddQueryParameter("idBoard", boardId);
 
-            RestResponse restResponse = restClient.Post(restRequest);
-            string listName = restResponse.Content.ToString();
-            var listInfo = JsonConvert.DeserializeObject<Objects>(listName);
-            todoListId = listInfo.Id;
-            HttpStatusCode = restResponse.StatusCode;
-            int statCode = (int)HttpStatusCode;
-            Assert.AreEqual(200, statCode);
+            RestClient restClient = new RestClient(BaseUrl);
+            RestRequest createListRequest = new RestRequest("lists/");
+            createListRequest.AddQueryParameter("key", "d67a55072cf473900fd3c72dddee7169");
+            createListRequest.AddQueryParameter("token", apiToken);
+            createListRequest.AddQueryParameter("name", "TODO");
+            createListRequest.AddQueryParameter("idBoard", boardId);
+
+            RestResponse createListResponse = restClient.Post(createListRequest);
+            var listInfo = JsonConvert.DeserializeObject<Objects>(createListResponse.Content);
+            var todoListId = listInfo?.Id;
+
+
+            RestRequest getListRequest = new RestRequest($"lists/{todoListId}");
+            getListRequest.AddQueryParameter("key", "d67a55072cf473900fd3c72dddee7169");
+            getListRequest.AddQueryParameter("token", apiToken);
+            RestResponse getListResponse = restClient.Get(getListRequest);
+            string retrievedListDetails = getListResponse.Content.ToString();
+            var retrievedList = JsonConvert.DeserializeObject<ListDetails>(retrievedListDetails);
+            string retrievedListId = retrievedList?.Id;
+            string retrievedListName = retrievedList?.Name;
+
+            Console.WriteLine($"List ID: {retrievedListId}");
+            Console.WriteLine($"List Name: {retrievedListName}");
+        }
+
+        [Test, Order(2)]
+
+        public void CreateNewTODOListNegative()
+        {
+
+            RestClient restClient = new RestClient(BaseUrl);
+            RestRequest createListRequest = new RestRequest("lists/");
+            createListRequest.AddQueryParameter("key", apiKey);
+            createListRequest.AddQueryParameter("token", apiToken);
+            createListRequest.AddQueryParameter("name", "TODO");
+            createListRequest.AddQueryParameter("idBoard", boardId);
+
+            RestResponse createListResponse = restClient.Post(createListRequest);
+            var listInfo = JsonConvert.DeserializeObject<Objects>(createListResponse.Content);
+            var todoListId = listInfo?.Id;
+
+
+            RestRequest getListRequest = new RestRequest($"lists/{todoListId}");
+            getListRequest.AddQueryParameter("key", apiKey);
+            getListRequest.AddQueryParameter("token", apiToken);
+            RestResponse getListResponse = restClient.Get(getListRequest);
+            string retrievedListDetails = getListResponse.Content.ToString();
+            var retrievedList = JsonConvert.DeserializeObject<ListDetails>(retrievedListDetails);
+            string retrievedListId = retrievedList?.Id;
+            string retrievedListName = retrievedList?.Name;
+
+            Console.WriteLine($"List ID: {retrievedListId}");
+            Console.WriteLine($"List Name: {retrievedListName}");
         }
 
         [Test, Order(3)]
         public void CreateNewDONEList()
         {
-            RestClient restClient = new RestClient(BaseUrl);
-            RestRequest restRequest = new RestRequest("lists/");
-            restRequest.AddQueryParameter("key", apiKey);
-            restRequest.AddQueryParameter("token", apiToken);
-            restRequest.AddQueryParameter("name", "DONE");
-            restRequest.AddQueryParameter("idBoard", boardId);
 
-            RestResponse restResponse = restClient.Post(restRequest);
-            string listName = restResponse.Content.ToString();
-            var listInfo = JsonConvert.DeserializeObject<Objects>(restResponse.Content);
-            doneListId = listInfo.Id;
-            HttpStatusCode = restResponse.StatusCode;
-            int statCode = (int)HttpStatusCode;
-            Assert.AreEqual(200, statCode);
+            RestClient restClient = new RestClient(BaseUrl);
+            RestRequest createDoneListRequest = new RestRequest("lists/");
+            createDoneListRequest.AddQueryParameter("key", apiKey);
+            createDoneListRequest.AddQueryParameter("token", apiToken);
+            createDoneListRequest.AddQueryParameter("name", "DONE");
+            createDoneListRequest.AddQueryParameter("idBoard", boardId);
+
+            RestResponse createDoneListResponse = restClient.Post(createDoneListRequest);
+            var listInfo = JsonConvert.DeserializeObject<Objects>(createDoneListResponse.Content);
+            var doneListId = listInfo?.Id;
+
+            RestRequest getDoneListRequest = new RestRequest($"lists/{doneListId}");
+            getDoneListRequest.AddQueryParameter("key", apiKey);
+            getDoneListRequest.AddQueryParameter("token", apiToken);
+            RestResponse getDoneListResponse = restClient.Get(getDoneListRequest);
+            string retrievedDoneListDetails = getDoneListResponse.Content.ToString();
+            var retrievedDoneList = JsonConvert.DeserializeObject<ListDetails>(retrievedDoneListDetails);
+
+            string retrievedDoneListId = retrievedDoneList?.Id;
+            string retrievedDoneListName = retrievedDoneList?.Name;
+            Console.WriteLine($"DONE List ID: {retrievedDoneListId}");
+            Console.WriteLine($"DONE List Name: {retrievedDoneListName}");
+
+
         }
 
         [Test, Order(4)]
-        public void CreateNewCard()
+        public void CreateNewCardToDo()
         {
             RestClient restClient = new RestClient(BaseUrl);
-            RestRequest restRequest = new RestRequest("cards/");
-            restRequest.AddQueryParameter("key", apiKey);
-            restRequest.AddQueryParameter("token", apiToken);
-            restRequest.AddQueryParameter("name", "Connor Card One");
-            restRequest.AddQueryParameter("idList", todoListId);
+            RestRequest createCardRequest = new RestRequest("cards/");
+            createCardRequest.AddQueryParameter("key", apiKey);
+            createCardRequest.AddQueryParameter("token", apiToken);
+            createCardRequest.AddQueryParameter("name", "Connor Card One");
+            createCardRequest.AddQueryParameter("idList", todoListId);
 
-            RestResponse restResponse = restClient.Post(restRequest);
+            RestResponse createCardResponse = restClient.Post(createCardRequest);
+            var cardInfo = JsonConvert.DeserializeObject<Objects>(createCardResponse.Content);
+            var cardId = cardInfo?.Id;
 
-            HttpStatusCode = restResponse.StatusCode;
-            int statCode = (int)HttpStatusCode;
-            Assert.AreEqual(200, statCode);
+            RestRequest getCardRequest = new RestRequest($"cards/{cardId}");
+            getCardRequest.AddQueryParameter("key", apiKey);
+            getCardRequest.AddQueryParameter("token", apiToken);
+            RestResponse getCardResponse = restClient.Get(getCardRequest);
+            string retrievedCardDetails = getCardResponse.Content.ToString();
+            var retrievedCard = JsonConvert.DeserializeObject<CardDetails>(retrievedCardDetails);
 
-            var cardInfo = JsonConvert.DeserializeObject<Objects>(restResponse.Content);
-            cardId = cardInfo?.Id;
+            string retrievedCardId = retrievedCard?.Id;
+            string retrievedCardName = retrievedCard?.Name;
+
+            Console.WriteLine($"Card ID: {retrievedCardId}");
+            Console.WriteLine($"Card Name: {retrievedCardName}");
+
         }
 
         [Test, Order(5)]
-        public void MoveCard()
+        public void CreateCardAndAddDescription()
         {
+
             {
                 RestClient restClient = new RestClient(BaseUrl);
-                RestRequest restRequest = new RestRequest($"cards/{cardId}/idList");
-                restRequest.AddQueryParameter("key", apiKey);
-                restRequest.AddQueryParameter("token", apiToken);
-                restRequest.AddQueryParameter("value", doneListId); 
+                RestRequest createCardRequest = new RestRequest("cards/");
+                createCardRequest.AddQueryParameter("key", apiKey);
+                createCardRequest.AddQueryParameter("token", apiToken);
+                createCardRequest.AddQueryParameter("name", "Add Description card");
+                createCardRequest.AddQueryParameter("idList", todoListId);
 
-                RestResponse restResponse = restClient.Put(restRequest); 
+                RestResponse createCardResponse = restClient.Post(createCardRequest);
+                var cardInfo = JsonConvert.DeserializeObject<Objects>(createCardResponse.Content);
+                var cardId = cardInfo?.Id;
 
-                HttpStatusCode = restResponse.StatusCode;
-                int statCode = (int)HttpStatusCode;
-                Assert.AreEqual(200, statCode);
+                RestRequest addDescriptionRequest = new RestRequest($"cards/{cardId}/desc");
+                addDescriptionRequest.AddQueryParameter("key", apiKey);
+                addDescriptionRequest.AddQueryParameter("token", apiToken);
+                addDescriptionRequest.AddParameter("value", "Connor Trello Description Test");
+
+                RestResponse addDescriptionResponse = restClient.Put(addDescriptionRequest);
+                var statusCodeAddDesc = addDescriptionResponse.StatusCode;
+                int statCodeAddDesc = (int)statusCodeAddDesc;
+                Console.WriteLine("Add Description Status Code: " + statCodeAddDesc);
+                Console.WriteLine($"Card '{cardId}' description: '{"Connor Trello Description Test"}'");
+
             }
-
         }
     }
 }
--- ConnorAPI/Drivers/Objects.cs
+++ ConnorAPI/Drivers/Objects.cs
@@ -10,4 +10,27 @@ namespace ConnorAPI.Drivers
     {
         public string Id { get; set; }
     }
+
+    public class BoardDetails
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        // Other properties as per your Trello API response for board details
+    }
+
+    public class ListDetails
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Pos { get; set; }
+        // Add more properties as per the response structure from the Trello API
+    }
+
+    public class CardDetails
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        // Add more properties as per the response structure from the Trello API
+    }
 }
